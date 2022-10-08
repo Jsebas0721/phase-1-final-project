4a67a9ef196383ace0598ec48ea8a7684ecf9736
sebastian medina <sebastianlopez_21@hotmail.com>
2022-10-08 13:48:39 -0400

    Initialize git

---
+++ js/index.js
@@ -0,0 +1,114 @@
+document.addEventListener("DOMContentLoaded", (e) => {
+
+e.preventDefault();
+fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s`)
+  .then(resp => resp.json())
+  .then(cocktail => {
+    console.log(cocktail.drinks); 
+    cocktail.drinks.forEach(element => renderCocktails(element));
+         
+  });
+   
+document.querySelector(".search-form").addEventListener('change', function(){
+
+  if(nameFilter.checked){
+    document.getElementById("input-text").placeholder="Enter a Cocktail Name...";
+}else if(liquorFilter.checked){
+    document.getElementById("input-text").placeholder="Enter a Liquor...";
+}else if(firstLetterFilter.checked){
+    document.getElementById("input-text").placeholder="Enter Letter...";
+}
+
+})
+
+document.querySelector(".search-form").addEventListener('submit', function(e){
+
+  e.preventDefault();
+
+  let nameFilter = document.getElementById("nameFilter");
+  let liquorFilter = document.getElementById("liquorFilter");
+  let firstLetterFilter = document.getElementById("firstLetterFilter");
+  let inputText = document.getElementById("input-text").value;
+  
+
+  document.getElementById("cocktail-container").innerHTML="";
+
+  if(nameFilter.checked){
+ 
+    fetch(`https://www.thecocktaildb.com/api/json/v2/9973533/search.php?s=${inputText}`)
+    .then(resp => resp.json())
+    .then(cocktail => {
+      console.log(cocktail);
+      let cocktailTitle = document.createElement('h3');
+      document.getElementById("cocktail-container").appendChild(cocktailTitle);
+      cocktailTitle.innerHTML= "Cocktail List"
+      cocktail.drinks.forEach(element => renderCocktails(element));
+    })
+  }else if(liquorFilter.checked){
+    
+    fetch(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${inputText}`)
+    .then(resp => resp.json())
+    .then(cocktail => {
+      console.log(cocktail);
+      //cre
+      let cocktailTitle = document.createElement('h3');
+      document.getElementById("cocktail-container").appendChild(cocktailTitle);
+      cocktailTitle.innerHTML= "Cocktail List"
+      cocktail.drinks.forEach(element => renderCocktails(element));
+    })
+  }else if(firstLetterFilter.checked){
+    
+
+    fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?f=${inputText}`)
+    .then(resp => resp.json())
+    .then(cocktail => {
+      console.log(cocktail);
+      let cocktailTitle = document.createElement('h3');
+      document.getElementById("cocktail-container").appendChild(cocktailTitle);
+      cocktailTitle.innerHTML= "Cocktail List"
+      cocktail.drinks.forEach(element => renderCocktails(element));
+    })
+  }
+
+  document.getElementById("input-text").value = "";
+
+})
+
+
+
+});
+  
+
+
+  function renderCocktails(cocktail){
+
+    let cocktailContainer = document.getElementById("cocktail-container");
+
+    let cocktailCard = document.createElement('div');
+    let cocktailName = document.createElement('h4');
+    let cocktailImage = document.createElement('img');
+    let ingredientsTitle = document.createElement('h5');
+    let cocktailIngredients= document.createElement('p');
+    let cocktailInstructions = document.createElement('p');
+
+    cocktailContainer.appendChild(cocktailCard);
+    cocktailCard.setAttribute("class", "cocktail-card")
+    cocktailCard.appendChild(cocktailName);
+    cocktailCard.appendChild(cocktailImage);
+    cocktailCard.appendChild(ingredientsTitle);
+    cocktailCard.appendChild(cocktailIngredients);
+    cocktailCard.appendChild(cocktailInstructions);
+
+    cocktailName.innerHTML = `${cocktail.strDrink}`; 
+    cocktailImage.setAttribute("src",`${cocktail.strDrinkThumb}`);
+    cocktailImage.setAttribute("class", "cocktail-img");
+    ingredientsTitle.innerHTML= "Ingredients: ";
+    cocktailIngredients.innerHTML= `${cocktail.strMeasure1} \u00A0 ${cocktail.strIngredient1}<br>
+                                    ${cocktail.strMeasure2} \u00A0 ${cocktail.strIngredient2}<br>
+                                    ${cocktail.strMeasure3} \u00A0 ${cocktail.strIngredient3}<br>
+                                    ${cocktail.strMeasure4} \u00A0 ${cocktail.strIngredient4}`;
+    cocktailInstructions.innerHTML =`${cocktail.strInstructions}`;
+
+
+}
+  
