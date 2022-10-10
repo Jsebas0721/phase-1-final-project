b7c495de187239a5f284d8a4652c23d2f42de4bb
sebastian medina <sebastianlopez_21@hotmail.com>
2022-10-10 12:05:43 -0400

    Adds inline comments to describe each action of the code

--- js/index.js
+++ js/index.js
@@ -1,67 +1,83 @@
+// Add event listner to document, run content after the page is fully loaded
 document.addEventListener("DOMContentLoaded", (e) => {
-
+// Prevent default behavior of the page
 e.preventDefault();
+
+// Send a fetch request to access cocktail data from Cocktial DataBase API
 fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s`)
   .then(resp => resp.json())
   .then(cocktail => {
-    //console.log(cocktail.drinks); 
-    cocktail.drinks.forEach(element => renderCocktails(element));
-         
+    //Access cocktail data retrieved by using an iterator (forEach)
+    //Invoke function to render each cocktail card in the page
+    cocktail.drinks.forEach(element => renderCocktails(element));     
   });
-   
+ 
+//Add even listener to Search Form (change)
+//When a filter is selected, change a its placeholder value 
 document.querySelector(".search-form").addEventListener('change', function(){
   if(nameFilter.checked){
     document.getElementById("input-text").placeholder="Enter a Cocktail Name...";
-}else if(liquorFilter.checked){
+  }else if(liquorFilter.checked){
     document.getElementById("input-text").placeholder="Enter a Liquor...";
-}else if(firstLetterFilter.checked){
+  }else if(firstLetterFilter.checked){
     document.getElementById("input-text").placeholder="Enter Letter...";
-}
-
+  }
 })
 
+//Add event listener to Search form (submit)
+//When a filter is selected, send a fetch request to retrieve specific data form Cocktail DataBase.
 document.querySelector(".search-form").addEventListener('submit', function(e){
-
+  //Prevent default behavior of the page
   e.preventDefault();
-
+  //Create variables for each filter, set each one equal to their HTML Element
   let nameFilter = document.getElementById("nameFilter");
   let liquorFilter = document.getElementById("liquorFilter");
   let firstLetterFilter = document.getElementById("firstLetterFilter");
+  //Create variable to store input text field value
   let inputText = document.getElementById("input-text").value;
   
+  //Clear cocktail container when a submit button is clicked!
   document.getElementById("cocktail-container").innerHTML="";
 
+  //When name filter is selected, Send a fetch request to (GET) cocktail Data filterd by name
   if(nameFilter.checked){
 
+    //Create header Element(cocktail title) and append it to cocktail container
     let cocktailTitle = document.createElement('h3');
     document.getElementById("cocktail-container").appendChild(cocktailTitle);
-
+    //Send a fetch request to access cocktail DataBase by name 
+    //Utilize inputText variable to filter the search
     fetch(`https://www.thecocktaildb.com/api/json/v2/9973533/search.php?s=${inputText}`)
     .then(resp => resp.json())
     .then(cocktail => {
-    
+      //Set the title for the page
       cocktailTitle.innerHTML= "Cocktail List";
+      //Use iterator (forEach) to access each item and render it using a function.
       cocktail.drinks.forEach(element => renderCocktails(element));
     })
+    //Catch seach error and display message to the user in the page!
     .catch(errorMessage => cocktailTitle.innerHTML = "Sorry, There is NOT matching results for that name!");
       
-    
+  //When liquor filter is selected, Send a fetch request to (GET) cocktail Data filterd by liquor  
   }else if(liquorFilter.checked){
 
+    //Create header Element(cocktail title) and append it to cocktail container
     let cocktailTitle = document.createElement('h3');
     document.getElementById("cocktail-container").appendChild(cocktailTitle);
-
+    //Send a fetch request to access cocktail DataBase by Liquor 
+    //Utilize inputText variable to filter the search
     fetch(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${inputText}`)
     .then(resp => resp.json())
     .then(cocktail => {
-
-      let cocktailIds = cocktail.drinks.map(element => element.idDrink);
-      // Create and set Cocktail Title
-      
+      //Set the title for the page
       cocktailTitle.innerHTML= "Cocktail List"
+      //Create a variable to store an Array of drink's ids
+      //Use iterator(map) to get a new arrray of drink's ids
+      let cocktailIds = cocktail.drinks.map(element => element.idDrink);
       
+        //Use a for in loop to access each index in cocktailIds' Array
         for(const drinkId in cocktailIds){
-          //console.log(cocktailIds[drinkId]);
+            
           fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${cocktailIds[drinkId]}`)
           .then(resp => resp.json())
           .then(drink=> {
