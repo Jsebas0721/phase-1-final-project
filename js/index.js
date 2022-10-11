567e11565574e29a0de015b520ff38892e907711
sebastian medina <sebastianlopez_21@hotmail.com>
2022-10-11 10:19:54 -0400

    Add inline comments to describre each action of the code (CONTINUED)

--- js/index.js
+++ js/index.js
@@ -77,40 +77,44 @@ document.querySelector(".search-form").addEventListener('submit', function(e){
       
         //Use a for in loop to access each index in cocktailIds' Array
         for(const drinkId in cocktailIds){
-            
+          //Send a fetch request to access Cocktail DataBase filtered by Liquor using each drink ID
           fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${cocktailIds[drinkId]}`)
           .then(resp => resp.json())
-          .then(drink=> {
-           
-  
-              drink.drinks.forEach(element => renderCocktails(element));
+          .then(cocktail=> {
+              //Access data using an iterator (forEach) to render each cocktail in the page
+              cocktail.drinks.forEach(element => renderCocktails(element));
           })
         }
     })
+    //Catch seach error and display message to the user in the page!
     .catch(errorMessage => cocktailTitle.innerHTML = "Sorry, There is NOT matching results for that Liquor type!");
+
+  //When First Letter filter is selected, Send a fetch request to (GET) cocktail Data filterd by the First Letter   
   }else if(firstLetterFilter.checked){
-    
+    //Create header Element(cocktail title) and append it to cocktail container
     let cocktailTitle = document.createElement('h3');
     document.getElementById("cocktail-container").appendChild(cocktailTitle);
-
+    //Send a fetch request to access cocktail DataBase by First Letter 
+    //Utilize inputText variable to filter the search
     fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?f=${inputText}`)
     .then(resp => resp.json())
     .then(cocktail => {
+      //Set the title for the page
       cocktailTitle.innerHTML= "Cocktail List"
+      //Access data using an iterator (forEach) to render each cocktail in the page
       cocktail.drinks.forEach(element => renderCocktails(element));
     })
+    //Catch seach error and display message to the user in the page!
     .catch(errorMessage => cocktailTitle.innerHTML = "Sorry, There is NOT matching results for that letter!");
   }
+  //Clear input text field value
   document.getElementById("input-text").value = "";
-
 })
-
-
 
 });
   
 
-
+  //Function used to render and display cocktail name, image, ingredient and instructions in the page
   function renderCocktails(cocktail){
 
     // Create cocktail container
@@ -127,29 +131,29 @@ document.querySelector(".search-form").addEventListener('submit', function(e){
     
     //Add cards to cocktail container
     cocktailContainer.appendChild(cocktailCard);
+    //Set attributes to elements, so they can be accessed and styled with CSS
     cocktailCard.setAttribute("class", "cocktail-card");
     ingredientsTitle.setAttribute("class", "ingredients-title");
     cocktailIngredients.setAttribute("class","cocktail-ingredients");
     cocktailInstructions.setAttribute("class", "cocktail-instructions");
-    //Add childs to cocktail card
+    //Add child Elements to cocktail card
     cocktailCard.appendChild(cocktailName);
     cocktailCard.appendChild(cocktailImage);
     cocktailCard.appendChild(ingredientsTitle);
     cocktailCard.appendChild(cocktailIngredients);
     cocktailCard.appendChild(cocktailInstructions);
 
+    //Create variable to store all the keys inside an object(cocktail)
     let cocktailKeys = Object.keys(cocktail);
-    console.log(cocktailKeys);
+    //Use forEach to iterate over the keys in a object(cocktail)
     cocktailKeys.forEach(key =>{
+      //If the key has a value equal to new, set that key a value of an empty string ' '
       if(cocktail[key] === null){
-        cocktail[key] ='';
-        
+        cocktail[key] ='';  
       }
     })
 
-    
-    console.log(cocktail);
-
+    //Set the innerHTML of each child element with their correspond properties from the cocktail Object
     cocktailName.innerHTML = `${cocktail.strDrink}`; 
     cocktailImage.setAttribute("src",`${cocktail.strDrinkThumb}`);
     cocktailImage.setAttribute("class", "cocktail-img");
@@ -160,7 +164,6 @@ document.querySelector(".search-form").addEventListener('submit', function(e){
                                     ${cocktail.strMeasure4}&ensp; ${cocktail.strIngredient4}<br>
                                     ${cocktail.strMeasure5}&ensp; ${cocktail.strIngredient5}`;
     cocktailInstructions.innerHTML =`${cocktail.strInstructions}`;
-   
-    
+      
 }
   
